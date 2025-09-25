086c19915ceadb367b16df48381b69c4fe45c7c1
aditya-rawat86 <rawatsunny747@gmail.com>
2025-09-25 11:50:37 +0530

    lab codes

---
+++ 25_09_2025/2_.c
@@ -0,0 +1,34 @@
+/*Write a program in C to count the total number of alphabets, digits and special characters in a string*/
+
+#include <stdio.h>
+#include <string.h>
+    int main()
+{
+    char str[100];
+    int i,alphabets=0,digits=0,special_characters=0;
+
+    printf("Enter a string = ");
+    gets(str);
+
+    for(i=0;str[i]!='\0';i++)
+    {
+        if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+        {
+            alphabets++;
+        }
+        else if(str[i]>='0' && str[i]<='9')
+        {
+            digits++;
+        }
+     else if(str[i] != ' ') 
+        {   
+            special_characters++;
+        }
+    }
+
+    printf("Total number of alphabets = %d\n",alphabets);
+    printf("Total number of digits = %d\n",digits);
+    printf("Total number of special characters = %d",special_characters);
+
+    return 0;
+}
